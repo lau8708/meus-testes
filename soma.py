4818480c9e70b78de694cc3b7451e9ee1bd2ab4d
Laudson Euller <laudsonps2@hotmail.com>
2025-05-04 22:50:18 -0300

    Função de soma criada

---
+++ soma.py
@@ -0,0 +1,11 @@
+def soma(a,b):
+
+    return a+b
+
+
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+
+resultado = soma(valor1,valor2)
+
+print(f"O resultado da soma é: {resultado}")
